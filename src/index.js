7b59ca1f988f32670c96accd92a07d92babc14bc
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-16 11:32:33 -0700

    Finished module Showed how to inject Javascript into the HTML code, and also how to use ES6 Template Literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//define variables to be inserted into HTML
+const fName = "Graydon";
+const lName = "Hall";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <h1> {"Hello " + `${fName} ${lName}`}</h1>
+    <h1> Hello {`${fName} ${lName}`}</h1>
+    <p>Your Lucky Number is {38 + 4}</p>
+    <p>Your Random Number of the day is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
